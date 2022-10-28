340d5eff1604b17d7184c5c3de31aa1fcc3868e3
ajay <ajaykota2003@gmail.com>
2022-10-28 11:17:40 +0530

    done with server

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
 const express = require('express')
-const nM = require("nodemailer")
-const transporter = nM.createTransport({
-    service:"hotmail",
-    auth : {
-        user:"spidersnipero@outlook.com",
-        pass:"spidy@Hero8"
-    }
-});
+
 
 const app = express()
 const bP  = require("body-parser")
@@ -16,22 +9,35 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname+"/index.html");
 });
 app.post("/",function(req,res){
+    const nM = require("nodemailer")
+    const mail = req.body.emailtosend;
+    console.log(mail);
+    const transporter = nM.createTransport({
+        service:"hotmail",
+        auth : {
+            user:"spidersnipero@outlook.com",
+            pass:"spidy@Hero8"
+        }
+    });
     const options = {
         from : "spidersnipero@outlook.com",
-        to :""+req.emailtosend,
+        to :mail,
         subject:"ANONYMOUS",
-        text : ""+req.texttosend,
+        text : req.body.texttosend,
     }
     transporter.sendMail(options,function(err,info){
         if(err){
             console.log(err);
+            res.sendFile(__dirname+"/error.html");
             return;
         }
+        else{
+            res.sendFile(__dirname+"/sent.html");
+        }
         console.log("Sent "+info.response);
+        transporter.close();
     })
-    console.log(req.body)
-    res.sendFile(__dirname+"/sent.html")
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
